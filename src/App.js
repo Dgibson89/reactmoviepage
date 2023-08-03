5f1269f7a58f033560d909addcf34b32114e119a
Dgibson89 <donovan.gibson89@gmail.com>
2023-08-03 14:48:01 -0500

    changed http to https in API url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=78c6515a";
+const API_URL = "https://www.omdbapi.com?apikey=78c6515a";
 
 const movie1 = {
   Title: "The Cabin in the Woods",
